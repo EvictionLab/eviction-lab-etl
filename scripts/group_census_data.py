3ebac4af239f2ab674bfa9950a0220824eb43aa9
pjsier <pjsier@gmail.com>
2017-10-16 11:13:11 -0500

    Improve group census data script speed

--- scripts/group_census_data.py
+++ scripts/group_census_data.py
@@ -1,23 +1,27 @@
 import sys
 import pandas as pd
+from functools import reduce
 
 CONTEXT_COLS = ['GEOID', 'name', 'parent-location']
-DATA_COLS = [
-    'evictions', 'population', 'area', 'average-household-size',
-    'renting-occupied-households', 'poverty-rate', 'eviction-rate'
-]
 
 if __name__ == '__main__':
     input_df = pd.read_csv(sys.stdin, dtype={'GEOID': 'object'})
-    year_data_cols = []
-    # Append -YEAR to each data column name
-    for year in input_df['year'].unique():
-        year_cols = ['{}-{}'.format(col, year) for col in DATA_COLS]
-        year_data_cols.extend(['{}-{}'.format(col, year) for col in DATA_COLS])
-        input_df[year_cols] = input_df.loc[input_df['year'] == year, DATA_COLS]
+    # Get non-context or year columns
+    data_cols = [c for c in input_df.columns.values if c not in CONTEXT_COLS + ['year']]
 
-    # Group the dataframe by GEOID, and take the max (not-null in this case) value
-    # for each newly-created data column
-    output_df = input_df.groupby('GEOID')[CONTEXT_COLS[1:] + year_data_cols].max()
-    output_df = pd.DataFrame(output_df).reset_index()
+    # Create context dataframe to join later
+    max_year = input_df['year'].max()
+    context_df = input_df.loc[input_df['year'] == max_year, CONTEXT_COLS]
+    context_df.set_index('GEOID', inplace=True)
+
+    # Create list of dataframes by year with -YEAR appended to data columns
+    year_df_list = []
+    for year in input_df['year'].unique():
+        year_df = input_df.loc[input_df['year'] == year, ['GEOID'] + data_cols]
+        year_df.set_index('GEOID', inplace=True)
+        year_df.columns = ['{}-{}'.format(col, year) for col in year_df.columns.values]
+        year_df_list.append(year_df)
+
+    # Join all year dataframes together with context on GEOID index
+    output_df = reduce(lambda x, y: x.join(y, how='left'), [context_df] + year_df_list)
     output_df.to_csv(sys.stdout, index=False)
