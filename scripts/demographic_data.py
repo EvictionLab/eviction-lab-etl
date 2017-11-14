6a176e12fc275341355a9aae956a40b2b9e6bc79
pjsier <pjsier@gmail.com>
2017-11-14 13:01:49 -0600

    Larger geographies working

--- scripts/demographic_data.py
+++ scripts/demographic_data.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import csv
+import numpy as np
 import pandas as pd
 from census import Census
 
@@ -13,6 +14,8 @@ ACS_VAR_MAP = {
     'B25111_001E': 'median-gross-rent',
     'B25001_001E': 'housing-units',
     'B25002_003E': 'vacant-housing-units',
+    'B25003_001E': 'occupied-housing-units',
+    'B25003_003E': 'renter-occupied-households',
     'B19013_001E': 'median-household-income',
     'B25077_001E': 'median-property-value',
     'B03002_012E': 'hispanic-pop',
@@ -25,11 +28,20 @@ ACS_VAR_MAP = {
     'B03002_009E': 'multiple-pop'
 }
 
+ACS_12_VAR_MAP = {
+    'B17001_002E': 'poverty-pop',
+    'B25111_001E': 'median-gross-rent',
+    'B19013_001E': 'median-household-income',
+    'B25077_001E': 'median-property-value'
+}
+
 CENSUS_90_VAR_MAP = {
     'ANPSADPI': 'name',
     'P0010001': 'population',
     'H0010001': 'housing-units',
-    'H0030002': 'renter-occupied-housing',
+    'H0020002': 'vacant-housing-units',
+    'H0020001': 'occupied-housing-units',
+    'H0030002': 'renter-occupied-households',
     # Average household size?
     'P0080001': 'hispanic-pop',
     'P0100001': 'white-pop',
@@ -44,7 +56,9 @@ CENSUS_00_VAR_MAP = {
     'NAME': 'name',
     'P001001': 'population',
     'H001001': 'housing-units',
-    'H004003': 'renter-occupied-housing',
+    'H003003': 'vacant-housing-units',
+    'H004001': 'occupied-housing-units',
+    'H004003': 'renter-occupied-households',
     'P017001': 'average-household-size',
     'P004002': 'hispanic-pop',
     'P004005': 'white-pop',
@@ -60,7 +74,9 @@ CENSUS_10_VAR_MAP = {
     'NAME': 'name',
     'P0030001': 'population',
     'H00010001': 'housing-units',
-    'H0040004': 'renter-occupied-housing',
+    'H0030003': 'vacant-housing-units',
+    'H0040001': 'occupied-housing-units',
+    'H0040004': 'renter-occupied-households',
     'H0120001': 'average-household-size',
     'P0040003': 'hispanic-pop',
     'P0050003': 'white-pop',
@@ -72,13 +88,55 @@ CENSUS_10_VAR_MAP = {
     'P0050009': 'multiple-pop'
 }
 
+# Columns to remove (mostly used to generate output columns)
+REMOVE_COLS = [
+    'GEOID',
+    'housing-units',
+    'occupied-housing-units',
+    'poverty-pop',
+    'hispanic-pop',
+    'white-pop',
+    'af-am-pop',
+    'am-ind-pop',
+    'asian-pop',
+    'nh-pi-pop',
+    'other-pop',
+    'multiple-pop',
+    'state',
+    'county',
+    'tract',
+    'place',
+    'block group'
+]
+
+NUMERIC_COLS = [
+    'population',
+    'poverty-pop',
+    'average-household-size',
+    'median-gross-rent',
+    'housing-units',
+    'vacant-housing-units',
+    'occupied-housing-units',
+    'renter-occupied-households',
+    'median-household-income',
+    'median-property-value',
+    'hispanic-pop',
+    'white-pop',
+    'af-am-pop',
+    'am-ind-pop',
+    'asian-pop',
+    'nh-pi-pop',
+    'other-pop',
+    'multiple-pop'
+]
+
 ACS_VARS = tuple(ACS_VAR_MAP.keys())
+ACS_12_VARS = tuple(ACS_12_VAR_MAP.keys())
 CENSUS_90_VARS = tuple(CENSUS_90_VAR_MAP.keys())
 CENSUS_00_VARS = tuple(CENSUS_00_VAR_MAP.keys())
 CENSUS_10_VARS = tuple(CENSUS_10_VAR_MAP.keys())
 
 c = Census(os.getenv('CENSUS_KEY'))
-
 
 STATE_FIPS = c.acs5.get(('NAME'), {'for': 'state:*'})
 STATE_FIPS_MAP = {s['state']: s['NAME'] for s in STATE_FIPS}
@@ -87,95 +145,67 @@ STATE_COUNTY_FIPS = c.acs5.get(('NAME'), {'for': 'county:*', 'in': 'state:*'})
 COUNTY_FIPS_MAP = {str(c['state']).zfill(2) + str(c['county']).zfill(3): c['NAME'] for c in STATE_COUNTY_FIPS}
 
 
+DATA_CLEANUP_FUNCS = {
+    'states': {
+        'geoid': lambda x: str(x['state']).zfill(2),
+        'parent-location': lambda x: 'USA'
+    },
+    'counties': {
+        'geoid': lambda x: str(x['state']).zfill(2) + str(x['county']).zfill(3),
+        'parent-location': lambda x: STATE_FIPS_MAP[x['state']]
+    },
+    'cities': {
+        'geoid': lambda x: str(x['state']).zfill(2) + str(x['place']).zfill(5),
+        'parent-location': lambda x: STATE_FIPS_MAP[x['state']]
+    },
+    'tracts': {
+        'geoid': lambda x: str(x['state']).zfill(2) + str(x['county']).zfill(3) + str(x['tract']).zfill(6),
+        'parent-location': lambda x: COUNTY_FIPS_MAP[str(x).zfill(2) + str(x).zfill(3)]
+    },
+    'block-groups': {
+        'geoid': lambda x: str(x['state']).zfill(2) + str(x['county']).zfill(3) + str(x['tract']).zfill(6) + str(x['block group']),
+        'parent-location': lambda x: x['tract'].zfill(6)
+    }
+}
+
+
 def state_county_sub_data(census_obj, geo_str, census_vars, year):
     geo_df_list = []
     for sc in STATE_COUNTY_FIPS:
+        # print(census_obj.get(
+        #     census_vars,
+        #     {'for': '{}:*'.format(geo_str.replace('-', ' ')[:-1]),
+        #      'in': 'county:{} state:{}'.format(sc['county'], sc['state'])},
+        #     year=year
+        # ))
         geo_df_list.append(pd.DataFrame(census_obj.get(
             census_vars,
-            {'for': '{}:*'.format(geo_str),
+            {'for': '{}:*'.format(geo_str.replace('-', ' ')[:-1]),
              'in': 'county:{} state:{}'.format(sc['county'], sc['state'])},
             year=year
         )))
     return pd.concat(geo_df_list)
 
 
-def state_geoid(row):
-    return str(row['state']).zfill(2)
-
-
-def county_geoid(row):
-    return (
-        str(row['state']).zfill(2) +
-        str(row['county']).zfill(3)
-    )
-
-
-def city_geoid(row):
-    return (
-        str(row['state']).zfill(2) +
-        str(row['place']).zfill(5)
-    )
-
-
-def tract_geoid(row):
-    return (
-        str(row['state']).zfill(2) +
-        str(row['county']).zfill(3) +
-        str(row['tract']).zfill(6)
-    )
-
-
-def block_group_geoid(row):
-    return (
-        str(row['state']).zfill(2) +
-        str(row['county']).zfill(3) +
-        str(row['tract']).zfill(6) +
-        str(row['block group'])
-    )
-
-
-def create_pct_cols(df):
-    pass
-
-
-def clean_state_df(df):
-    df['GEOID'] = df.apply(state_geoid, axis=1)
-    df['parent-location'] = 'USA'
-    df_cols = ['GEOID'] + [c for c in df.columns.values() if c not in ['GEOID', 'state']]
-    df = df[df_cols].copy()
+def generated_cols(df):
+    df['pct-renter-occupied'] = df.apply(lambda x: x['renter-occupied-households'] / x['occupied-housing-units'] if x['occupied-housing-units'] > 0 else 0, axis=1)
+    if 'poverty-pop' in df.columns.values:
+        df['poverty-rate'] = df.apply(lambda x: x['poverty-pop'] / x['population'] if x['population'] > 0 else 0, axis=1)
+    else:
+        # Should nulls be handled this way here?
+        df['poverty-rate'] = np.nan
+    for dem in ['hispanic', 'white', 'af-am', 'am-ind', 'asian', 'nh-pi', 'other', 'multiple']:
+        if dem in df.columns.values:
+            df['pct-{}'.format(dem)] = df.apply(lambda x: x['{}-pop'.format(dem)] / x['population'] if x['population'] > 0 else 0, axis=1)
     return df
 
 
-def clean_county_df(df):
-    df['GEOID'] = df.apply(county_geoid, axis=1)
-    df['parent-location'] = df['state'].apply(lambda x: STATE_FIPS_MAP[x])
-    df_cols = ['GEOID'] + [c for c in df.columns.values() if c not in ['GEOID', 'state', 'county']]
-    df = df[df_cols].copy()
-    return df
-
-
-def clean_city_df(df):
-    df['GEOID'] = df.apply(city_geoid, axis=1)
-    df_cols = ['GEOID'] + [c for c in df.columns.values() if c not in ['GEOID', 'state', 'place']]
-    df['parent-location'] = df['state'].apply(lambda x: STATE_FIPS_MAP[x])
-    df = df[df_cols].copy()
-    return df
-
-
-def clean_tract_df(df):
-    df['GEOID'] = df.apply(tract_geoid, axis=1)
-    df_cols = ['GEOID'] + [c for c in df.columns.values() if c not in ['GEOID', 'state', 'county', 'tract']]
-    df['parent-location'] = df.apply(lambda x: COUNTY_FIPS_MAP[str(x).zfill(2) + str(x).zfill(3)], axis=1)
-    df = df[df_cols].copy()
-    return df
-
-
-def clean_block_group_df(df):
-    df['GEOID'] = df.apply(block_group_geoid, axis=1)
-    df_cols = ['GEOID'] + [c for c in df.columns.values() if c not in ['GEOID', 'state', 'county', 'tract', 'block group']]
-    df['parent-location'] = df['tract'].apply(lambda x: x.zfill(6))
-    df = df[df_cols].copy()
-    return df
+def clean_data_df(df, geo_str):
+    df['GEOID'] = df.apply(DATA_CLEANUP_FUNCS[geo_str]['geoid'], axis=1)
+    df['parent-location'] = df.apply(DATA_CLEANUP_FUNCS[geo_str]['parent-location'], axis=1)
+    df[NUMERIC_COLS] = df[NUMERIC_COLS].apply(pd.to_numeric)
+    df = generated_cols(df)
+    return df[['GEOID'] + [c for c in df.columns.values if c not in REMOVE_COLS]].copy()
 
 
 def get_90_data(geo_str):
@@ -186,6 +216,10 @@ def get_90_data(geo_str):
     elif geo_str == 'counties':
         census_df = pd.DataFrame(c.sf1.get(
             CENSUS_90_VARS, {'for': 'county:*', 'in': 'state:*'}, year=1990
+        ))
+    elif geo_str == 'cities':
+        census_df = pd.DataFrame(c.sf1.get(
+            CENSUS_90_VARS, {'for': 'place:*', 'in': 'state:*'}, year=1990
         ))
     else:
         census_df = state_county_sub_data(c.sf1, geo_str, CENSUS_90_VARS, 1990)
@@ -214,6 +248,13 @@ def get_00_data(geo_str):
         acs_df = pd.DataFrame(c.acs5.get(
             ACS_VARS, {'for': 'county:*', 'in': 'state:*'}, year=2009
         ))
+    elif geo_str == 'cities':
+        census_df = pd.DataFrame(c.sf1.get(
+            CENSUS_00_VARS, {'for': 'place:*', 'in': 'state:*'}, year=2000
+        ))
+        acs_df = pd.DataFrame(c.acs5.get(
+            ACS_VARS, {'for': 'place:*', 'in': 'state:*'}, year=2009
+        ))
     else:
         census_df = state_county_sub_data(c.sf1, geo_str, CENSUS_00_VARS, 2000)
         acs_df = state_county_sub_data(c.acs5, geo_str, ACS_VARS, 2009)
@@ -241,6 +282,9 @@ def get_10_data(geo_str):
         census_df = pd.DataFrame(c.sf1.get(
             CENSUS_10_VARS, {'for': 'state:*'}, year=2010
         ))
+        acs_12_df = pd.DataFrame(c.acs5.get(
+            ACS_12_VARS, {'for': 'state:*'}, year=2012
+        ))
         acs_df = pd.DataFrame(c.acs5.get(
             ACS_VARS, {'for': 'state:*'}, year=2015
         ))
@@ -248,15 +292,45 @@ def get_10_data(geo_str):
         census_df = pd.DataFrame(c.sf1.get(
             CENSUS_10_VARS, {'for': 'county:*', 'in': 'state:*'}, year=2010
         ))
+        acs_12_df = pd.DataFrame(c.acs5.get(
+            ACS_12_VARS, {'for': 'county:*', 'in': 'state:*'}, year=2012
+        ))
         acs_df = pd.DataFrame(c.acs5.get(
             ACS_VARS, {'for': 'county:*', 'in': 'state:*'}, year=2015
         ))
+    elif geo_str == 'cities':
+        census_df = pd.DataFrame(c.sf1.get(
+            CENSUS_10_VARS, {'for': 'place:*', 'in': 'state:*'}, year=2010
+        ))
+        acs_12_df = pd.DataFrame(c.acs5.get(
+            ACS_12_VARS, {'for': 'place:*', 'in': 'state:*'}, year=2012
+        ))
+        acs_df = pd.DataFrame(c.acs5.get(
+            ACS_VARS, {'for': 'place:*', 'in': 'state:*'}, year=2015
+        ))
     else:
         census_df = state_county_sub_data(c.sf1, geo_str, CENSUS_10_VARS, 2010)
+        acs_12_df = state_county_sub_data(c.acs5, geo_str, ACS_12_VARS, 2012)
         acs_df = state_county_sub_data(c.acs5, geo_str, ACS_VARS, 2015)
 
     census_df.rename(columns=CENSUS_10_VAR_MAP, inplace=True)
+    acs_12_df.rename(columns=ACS_12_VAR_MAP, inplace=True)
+
+    # Merge vars that are only in ACS to 2010 census
+    if geo_str == 'states':
+        acs_merge_keys = ['state']
+    elif geo_str == 'counties':
+        acs_merge_keys = ['state', 'county']
+    elif geo_str == 'cities':
+        acs_merge_keys = ['state', 'place']
+    elif geo_str == 'tracts':
+        acs_merge_keys  = ['state', 'county', 'tract']
+    elif geo_str == 'block-groups':
+        acs_merge_keys = ['state', 'county', 'tract', 'block group']
+    census_df = census_df.merge(acs_12_df, on=acs_merge_keys, how='left')
+
     acs_df.rename(columns=ACS_VAR_MAP, inplace=True)
+
     census_df['year'] = 2010
     acs_df_list = []
     for year in range(2011, 2017):
@@ -281,4 +355,5 @@ if __name__ == '__main__':
     else:
         raise ValueError('An invalid argument was supplied')
 
-    data_df.to_csv(sys.stdout, index=False, quoting=csv.QUOTE_NONE)
+    data_df = clean_data_df(data_df, geo_str).round(2)
+    data_df.to_csv(sys.stdout, index=False, quoting=csv.QUOTE_NONNUMERIC)
