e913dfdee4e0d6e1664a1df363e8ba9312ccd64e
pjsier <pjsier@gmail.com>
2017-11-16 19:37:27 -0600

    Escape unicode

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-git pull origin master
+git fetch origin master
+git reset --hard FETCH_HEAD
+git clean -df
 make $1
 make deploy
--- scripts/csvjoin.py
+++ scripts/csvjoin.py
@@ -7,10 +7,14 @@ if __name__ == '__main__':
     join_keys = sys.argv[1].split(',')
     dtypes = {k: 'object' for k in join_keys}
     dtypes['name'] = 'object'
+    dtypes['parent-location'] = 'object'
     df_list = []
     for filename in sys.argv[2:]:
         df = pd.read_csv(filename, dtype=dtypes)
         df.set_index(join_keys, inplace=True)
+        for c in ['name', 'parent-location']:
+            if c in df.columns.values:
+                df[c] = df[c].apply(lambda x: str(x).encode('ascii', 'ignore').decode('ascii'))
         df_list.append(df)
     output_df = reduce(lambda x, y: x.join(y, how='left'), df_list)
     output_df.to_csv(sys.stdout)
