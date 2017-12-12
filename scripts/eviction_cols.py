120514a6cc4de0eba87fa21c3a469193b931407e
pjsier <pjsier@gmail.com>
2017-12-11 21:04:05 -0500

    Drop evictions-per-day

--- scripts/eviction_cols.py
+++ scripts/eviction_cols.py
@@ -10,9 +10,5 @@ if __name__ == '__main__':
     # Generate eviction columns
     df['eviction-rate'] = df['evictions'] / (df['renter-occupied-households'] / 100)
     df['eviction-filing-rate'] = df['eviction-filings'] / (df['renter-occupied-households'] / 100)
-    # Check leap year for per day calculation
-    df['evictions-per-day'] = np.where(
-        df['year'] % 4 == 0, df['evictions'] / 366, df['evictions'] / 365
-    )
 
     df.round(2).to_csv(sys.stdout, index=False, quoting=csv.QUOTE_NONNUMERIC)
