dcd2f755b1c0f523c896224cd613d447e4e317a8
pjsier <pjsier@gmail.com>
2018-01-18 12:32:36 -0600

    Create US average eviction data

---
+++ scripts/create_us_average.py
@@ -0,0 +1,15 @@
+import sys
+import json
+import pandas as pd
+
+if __name__ == '__main__':
+    df = pd.read_csv(
+        sys.argv[1],
+        engine='python',
+        dtype={'GEOID': 'object', 'n': 'object', 'pl': 'object'}
+    )
+
+    # data_cols = [c for c in df.columns.values if '-' in c]
+    eviction_cols = [c for c in df.columns.values if 'er-' in c or 'efr-' in c]
+    us_avg = df[eviction_cols].to_dict(orient='records')[0]
+    json.dump(us_avg, sys.stdout)
