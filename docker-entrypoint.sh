29586d287725940f554ccd53e8c9ff7641c2fe60
Lane <laneolson@gmail.com>
2018-11-19 09:38:38 -0800

    drop simultaneous jobs from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [[ $1 == deploy_public_data ]] || [[ $1 == deploy_data ]]; then
     make $1
 elif [[ $1 == *demographics* ]]; then
     if [[ $1 == *block-groups* ]]; then
-        make -f demographics.mk -j 25 census/00/block-groups.csv census/10/block-groups.csv
+        make -f demographics.mk census/00/block-groups.csv census/10/block-groups.csv
     fi
     make -f demographics.mk $1
     make -f demographics.mk deploy
